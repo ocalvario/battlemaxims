57ad3d45ca28b474005c19b38f08cbb235b5f2b9
ocalvario <oswald.calvario@gmail.com>
2020-07-28 19:10:38 -0400

    Creates initial fetch request

--- js-frontend/index.js
+++ js-frontend/index.js
@@ -1,1 +1,25 @@
-console.log("in index.js")
+const endPoint = "http://localhost:3000/api/v1/battles"
+
+document.addEventListener('DOMContentLoaded', () => {
+    getBattles()
+})
+
+function getBattles () {
+    fetch (endPoint)
+    .then(response => response.json())
+    .then( battles => {
+        battles.data.forEach(battle => {
+         const battleMarkup = `
+          <div data-id=${battle.id}>
+            <img src=${battle.attributes.image_url} height="200" width="250">
+            <h3>${battle.attributes.title}</h3>
+            <h5>${battle.attributes.country.name}</h5>
+            <p>${battle.attributes.description}</p>
+
+            <button data-id=${battle.id}>edit</button>
+          </div>
+          <br><br>`;
+          document.querySelector('#battle-container').innerHTML += battleMarkup
+        })
+    })
+}
