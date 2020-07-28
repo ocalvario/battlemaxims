10174d863f6ddcae17d182de45187b96c7609123
ocalvario <oswald.calvario@gmail.com>
2020-07-28 15:09:05 -0400

    Adds controller actions and create route

--- app/controllers/api/v1/battles_controller.rb
+++ app/controllers/api/v1/battles_controller.rb
@@ -1,2 +1,23 @@
 class Api::V1::BattlesController < ApplicationController
+
+    def index
+        battles = Battle.all
+        render json: battles
+    end
+
+    def create
+        battle = Battle.new(battle_params)
+        if battle.save
+            render json: battle, status: :accepted
+        else
+            render json: {errors: battle.errors.full_messages}
+        end
+    end
+
+    private
+
+    def battle_params
+        params.require(:battle).permit(:title, :description, :image_url, :country_id)
+    end
+
 end
