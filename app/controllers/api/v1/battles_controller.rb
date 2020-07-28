b223a149cbe1e2f7beb4e155c051ba14bf78d768
ocalvario <oswald.calvario@gmail.com>
2020-07-28 15:45:35 -0400

    Adds serializers

--- app/controllers/api/v1/battles_controller.rb
+++ app/controllers/api/v1/battles_controller.rb
@@ -2,7 +2,9 @@ class Api::V1::BattlesController < ApplicationController
 
     def index
         battles = Battle.all
-        render json: battles
+        # render json: battles
+        render json: BattleSerializer.new(battles)
+        
     end
 
     def create
@@ -10,14 +12,14 @@ class Api::V1::BattlesController < ApplicationController
         if battle.save
             render json: battle, status: :accepted
         else
-            render json: {errors: battle.errors.full_messages}
+            render json: {errors: battle.errors.full_messages}, status: :unprocessible_entity
         end
     end
 
     private
 
     def battle_params
-        params.require(:battle).permit(:title, :description, :image_url, :country_id)
+        params.require(:battle).permit(:title, :description, :image_url, :country)
     end
 
 end
